8ae9c70f919a7b1063730b98487d394946844f1c
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-08-18 16:42:45 -0700

    Update scrapi.py (#4)
    * Update scrapi.py
    
    * Update scrapi.py (again)

--- scrapi.py
+++ scrapi.py
@@ -2,17 +2,37 @@
 
 print("loading...")
 import sys
-webargumen = sys.argv[1]
+webargumen = sys.argv[1] # allows this to connect with web ig
 print("credit:")
-print("Code made by ScolderCreations. API made by the Scratch Team.")
+print("Code made by ScolderCreations. API made by the Scratch Team.") # don't remove this
 print()
-import requests
-INFO = "lol wut"
-wanna = "yes"
+import requests # NOTE: you'll need to install this module first
+INFO = "lol wut" # impossible value
+wanna = "yes" # prevents program from instantly ending
+
+def updinf(parameter):
+  INFO = INFO + parameter
+
 if "preset" in webargumen: # preset argument
   datType = input("What type of data do you want? 1: User 2: Project ") 
   if "1" in datType:
     INFO = "/users/" + input("What is your username? ")
+    datType = input("Data? 1: Studio List 2: Project List 3: Followers 4: Favorites 5: General ") # additional user data
+    if "1" in datType:
+      updinf("/studios/curate/")
+    elif "2" in datType:
+      updinf("/projects/")
+    elif "3" in datType:
+      updinf("/followers/")
+    elif "4" in datType:
+      updinf("/favorites/")
+    elif "5" in datType:
+      updinf("/")
+    else:
+      print("...")
+      print("You IDIOT.")
+      updinf("/")
+      print()
   elif "2" in datType:
     INFO = "/projects/" + input("insert a Project ID: ")
   else:
@@ -20,13 +40,15 @@ if "preset" in webargumen: # preset argument
 elif "y" in input("Use a preset? "): # nonpreset argument
   datType = input("What type of data do you want? 1: User 2: Project ")
   if "1" in datType:
-    INFO = "/users/" + input("What is your username? ")
+    INFO = "/users/" + input("What is your username? ") + "/"
   elif "2" in datType:
-    INFO = "/projects/" + input("insert a Project ID: ")
+    INFO = "/projects/" + input("insert a Project ID: ") + "/"
   else:
     INFO = input("Please insert parameters ")
 else:
   INFO = input("Please insert the parameters ")
+if input("Any extra parameters? y/n ") = "y":
+  updinf(input("Custom paramaters: "))
 url = 'https://api.scratch.mit.edu' + INFO
 print()
 print("url: \"" + url + "\"")
